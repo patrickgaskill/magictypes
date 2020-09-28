f7e8a9d56035014d7f2c0025aa14f96482d6ff21
Patrick Gaskill <patrick.gaskill@gmail.com>
2020-09-27 22:47:28 -0500

    Token parsing

--- magictypes.py
+++ magictypes.py
@@ -1,8 +1,9 @@
-import json
-import types
-import re
-import datetime
 import csv
+import datetime
+import itertools
+import json
+import re
+from types import SimpleNamespace
 
 
 def load_allprintings():
@@ -25,6 +26,81 @@ def has_every_creature_type(Card):
     return False
 
 
+def make_token_pattern(cardtypes):
+    colors = '|'.join(
+        ['white', 'blue', 'black', 'red', 'green', 'colorless', 'and'])
+    numbers = '|'.join([
+        'an?', r'a\ number\ of', 'X', 'two', 'three', 'four', 'five', 'six',
+        'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen'
+    ])
+    types = '|'.join(cardtypes.keys())
+    supertypes = '|'.join(cardtypes['artifact']['superTypes'])
+    subtypes = '|'.join(
+        itertools.chain(*[v['subTypes'] for v in cardtypes.values()]))
+
+    pattern = re.compile(
+        rf"""create\s
+             (?:(?P<legendary_name>[^,]+),\s)?
+             (?:{numbers})\s
+             (?:tapped\s)?
+             (?P<supertypes>(?:(?:{supertypes})\s)+)?
+             (?:(?P<power>[\dX*]+)\/(?P<toughness>[\dX*]+)\s)?
+             (?P<colors1>(?:(?:{colors})\s)+)?
+             (?P<subtypes>(?:(?:{subtypes})\s)+)?
+             (?P<types>(?:(?:{types})\s)+)
+             tokens?
+             (?:\ that’s\ (?P<colors2>(?:(?:{colors})[., ]+)+))?""",
+        re.IGNORECASE | re.VERBOSE)
+    return pattern
+
+
+def extract_tokens(Card, pattern):
+    if (Card.name == 'Master of the Wild Hunt Avatar'):
+        return [
+            SimpleNamespace(types=['Creature'],
+                            subtypes=['Wolf'],
+                            supertypes=[]),
+            SimpleNamespace(types=['Creature'],
+                            subtypes=['Antelope'],
+                            supertypes=[]),
+            SimpleNamespace(types=['Creature'],
+                            subtypes=['Cat'],
+                            supertypes=[]),
+            SimpleNamespace(types=['Creature'],
+                            subtypes=['Rhino'],
+                            supertypes=[])
+        ]
+
+    if not hasattr(Card, 'text'):
+        return []
+
+    matches = re.findall(pattern, Card.text)
+    tokens = []
+    for m in matches:
+        _, supertypes, _, _, _, subtypes, types, _ = m
+
+        types_list = [t.capitalize() for t in types.strip().split(' ')
+                      ] if len(types) > 0 else []
+        subtypes_list = [s.capitalize() for s in subtypes.strip().split(' ')
+                         ] if len(subtypes) > 0 else []
+        supertypes_list = [
+            s.capitalize() for s in supertypes.strip().split(' ')
+        ] if len(supertypes) > 0 else []
+
+        tokens.append(
+            SimpleNamespace(types=types_list,
+                            subtypes=subtypes_list,
+                            supertypes=supertypes_list))
+
+    if re.search(r'create a Food token', Card.text, re.IGNORECASE):
+        tokens.append(
+            SimpleNamespace(types=['Artifact'],
+                            subtypes=['Food'],
+                            supertypes=[]))
+
+    return tokens
+
+
 def sort_key(Card, Set):
     release_date = datetime.date.fromisoformat(Set.releaseDate)
     parsed_number = int(re.sub(r'[^\d]+', '', Card.number))
@@ -34,11 +110,12 @@ def sort_key(Card, Set):
 def main():
     allprintings = load_allprintings()
     cardtypes = load_cardtypes()
-
+    token_pattern = make_token_pattern(cardtypes)
     card_firsts = {}
+    token_firsts = {}
 
     for set_model in allprintings.values():
-        Set = types.SimpleNamespace(**set_model)
+        Set = SimpleNamespace(**set_model)
 
         if Set.type in ('funny', 'memorabilia', 'promo'):
             continue
@@ -47,7 +124,7 @@ def main():
             continue
 
         for card_model in Set.cards:
-            Card = types.SimpleNamespace(**card_model)
+            Card = SimpleNamespace(**card_model)
 
             if Card.borderColor == 'silver':
                 continue
@@ -60,6 +137,13 @@ def main():
             # Does it add or change types in play or after activation?
 
             # Does it make any tokens?
+            tokens = extract_tokens(Card, token_pattern)
+            for token in tokens:
+                key = (tuple(token.supertypes), tuple(token.types),
+                       tuple(token.subtypes))
+                if key not in token_firsts or sort_key(Card, Set) < sort_key(
+                        token_firsts[key][1], token_firsts[key][2]):
+                    token_firsts[key] = (token, Card, Set)
 
             if is_every_creature_type:
                 noncreature_subtypes = tuple(
@@ -79,9 +163,19 @@ def main():
 
     with open('card_firsts.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        values = card_firsts.values()
-        values.sort(key=lambda v: sort_key(*v))
-        rows = [(c.type, c.name, c.setCode, c.number) for (c, _) in values]
+        rows = [(c.type, c.name, c.setCode, c.number)
+                for c, _ in sorted(card_firsts.values(),
+                                   key=lambda v: sort_key(*v))]
+        writer.writerows(rows)
+
+    print(len(token_firsts.keys()), 'tokens found.')
+
+    with open('token_firsts.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        rows = [(t.types, t.subtypes, t.supertypes, c.name, c.setCode,
+                 c.number)
+                for t, c, _ in sorted(token_firsts.values(),
+                                      key=lambda v: sort_key(v[1], v[2]))]
         writer.writerows(rows)
 
 
--- test_magictypes.py
+++ test_magictypes.py
@@ -2,37 +2,50 @@ import unittest
 import types
 import magictypes
 
+allprintings = magictypes.load_allprintings()
+cardtypes = magictypes.load_cardtypes()
+
 
 class CardTestCase(unittest.TestCase):
     def setUp(self):
-        self.allprintings = magictypes.load_allprintings()
-        self.cardtypes = magictypes.load_cardtypes()
+        self.allprintings = allprintings
+        self.cardtypes = cardtypes
 
     def find_card(self, set_code, card_name):
         card_model = next((c for c in self.allprintings[set_code]['cards']
                            if c['name'] == card_name), None)
         return types.SimpleNamespace(**card_model) if card_model else None
 
-    def test_changelings(self):
+
+class ChangelingsTestCase(CardTestCase):
+    def test_mistform_ultimus(self):
         self.assertTrue(
             magictypes.has_every_creature_type(
                 self.find_card('LGN', 'Mistform Ultimus')))
 
+    def test_amorphous_axe(self):
         self.assertFalse(
             magictypes.has_every_creature_type(
                 self.find_card('MH1', 'Amorphous Axe')))
 
+    def test_runed_stalactite(self):
         self.assertFalse(
             magictypes.has_every_creature_type(
                 self.find_card('LRW', 'Runed Stalactite')))
 
+    def test_crib_swap(self):
         self.assertTrue(
             magictypes.has_every_creature_type(
                 self.find_card('LRW', 'Crib Swap')))
 
+    def test_amoeboid_changeling(self):
         self.assertTrue(
             magictypes.has_every_creature_type(
                 self.find_card('LRW', 'Amoeboid Changeling')))
+
+
+class TokensTestCase(CardTestCase):
+    pass
 
 
 if __name__ == '__main__':
