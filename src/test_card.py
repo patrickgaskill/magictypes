1f71d532b295b7209fe581a5a94325868c7a1a75
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-04-04 02:00:51 -0500

    Add tests

---
+++ src/test_card.py
@@ -0,0 +1,34 @@
+import pytest
+import magictypes
+from operator import attrgetter
+
+
+@pytest.fixture(scope="session")
+def cards():
+    return magictypes.load_cards()
+
+
+@pytest.fixture
+def get_card(cards):
+    def _get_card(name):
+        return next(
+            (c for c in cards if c.name == name),
+            None,
+        )
+
+    return _get_card
+
+
+@pytest.mark.parametrize(
+    "name,expected",
+    [
+        ("Mistform Ultimus", True),
+        ("Amorphous Axe", False),
+        ("Runed Stalactite", False),
+        ("Crib Swap", True),
+        ("Amoeboid Changeling", True),
+        ("Moritte of the Frost", True),
+    ],
+)
+def test_is_every_creature_type(get_card, name, expected):
+    assert get_card(name).is_every_creature_type == expected
