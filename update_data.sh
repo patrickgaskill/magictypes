0569cf3f360790618b4c36aeef885f669c14f5e6
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-05-22 02:08:41 -0500

    Add mkdir to script

--- update_data.sh
+++ update_data.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+mkdir -p data
 curl https://mtgjson.com/api/v5/AllIdentifiers.json.zip | tar xv -C data
 curl https://mtgjson.com/api/v5/EnumValues.json.zip | tar xv -C data
 curl https://mtgjson.com/api/v5/SetList.json.zip | tar xv -C data
