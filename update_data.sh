79e98553a5626f8ba12af6c643b0b3fadafefa0e
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-05-22 01:57:33 -0500

    Add curl data script

---
+++ update_data.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+curl https://mtgjson.com/api/v5/AllIdentifiers.json.zip | tar xv -C data
+curl https://mtgjson.com/api/v5/EnumValues.json.zip | tar xv -C data
+curl https://mtgjson.com/api/v5/SetList.json.zip | tar xv -C data
