fb6f27536b45c0b4dce467e8347488ce06a82eff
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-05-25 18:29:26 -0500

    Add CardTypes

--- update_data.sh
+++ update_data.sh
@@ -5,6 +5,7 @@ declare -a FileArray=(
   "https://mtgjson.com/api/v5/AllIdentifiers.json.zip"
   "https://mtgjson.com/api/v5/EnumValues.json.zip"
   "https://mtgjson.com/api/v5/SetList.json.zip"
+  "https://mtgjson.com/api/v5/CardTypes.json.zip"
 )
 
 for file in ${FileArray[@]}; do
