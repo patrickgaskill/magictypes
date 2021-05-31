5dd986537e2d1c5823ba9c29455860743e08a939
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-05-30 23:18:08 -0500

    More tests working

--- src/lark_token_extractor.py
+++ src/lark_token_extractor.py
@@ -21,7 +21,7 @@ class TokenExtractor:
     def __init__(self):
         with open("tokens2.lark") as f:
             self.grammar = f.read()
-        self.parser = Lark(self.grammar, ambiguity="explicit")
+        self.parser = Lark(self.grammar, debug=True)
 
     def _tree_to_tokens(self, tree: lark.Tree) -> list[MagicToken]:
         tokens = []
@@ -32,9 +32,9 @@ class TokenExtractor:
             for t in creator.scan_values(lambda v: isinstance(v, lark.Token)):
                 if t.type == "COLOR" and t in self.color_map:
                     characteristics["colors"].append(self.color_map[t])
-                elif t.type == "SUBTYPES":
+                elif t.type == "SUBTYPE":
                     characteristics["subtypes"].append(str(t))
-                elif t.type == "TYPES":
+                elif t.type == "TYPE":
                     characteristics["types"].append(str(t).capitalize())
                 elif t.type == "POWER":
                     characteristics["power"] = str(t)
@@ -42,29 +42,38 @@ class TokenExtractor:
                     characteristics["toughness"] = str(t)
                 elif t.type == "RULES_TEXT":
                     characteristics["text"] = str(t)
-
+                elif t.type == "TOKEN_NAME":
+                    characteristics["name"] = str(t)
+                elif t.type == "LEGENDARY" and t == "legendary":
+                    characteristics["supertypes"].append(str("Legendary"))
+            print("characteristics=", characteristics)
             tokens.append(MagicToken(**characteristics))
         return tokens
 
-    def extract(self, card: Card) -> list[MagicToken]:
-        if not hasattr(card, "text"):
-            return []
-
-        tree = self.parser.parse(card.text)
+    def extract_from_text(self, text: str) -> list[MagicToken]:
+        tree = self.parser.parse(text)
         print(tree)
         print(tree.pretty())
         tokens = self._tree_to_tokens(tree)
         print(tokens)
         return tokens
 
+    def extract_from_card(self, card: Card) -> list[MagicToken]:
+        if not hasattr(card, "text"):
+            return []
+        return self.extract_from_text(card.text)
+
 
 if __name__ == "__main__":
     extractor = TokenExtractor()
     cards = load_cards()
-    aerie = next(
-        (c for c in cards if c.name == "Aerie Worshippers"),
+    card = next(
+        (c for c in cards if c.name == "Abhorrent Overlord"),
         None,
     )
-    print(aerie.text)
-    tokens = extractor.extract(aerie)
-    print(tokens)
+    print(card.text)
+    tokens = extractor.extract_from_card(card)
+    text = "Flying\nWhen Abhorrent Overlord enters the battlefield, create a number of 1/1 black Harpy creature tokens with flying equal to your devotion to black. (Each {B} in the mana costs of permanents you control counts toward your devotion to black.)\nAt the beginning of your upkeep, sacrifice a creature."
+    # text = "Dark Depths enters the battlefield with ten ice counters on it. {3}: Remove an ice counter from Dark Depths. When Dark Depths has no ice counters on it, sacrifice it. If you do, create a 20/20 black Avatar creature token with flying and indestructible."
+    tokens = extractor.extract_from_text(text)
+    # print(tokens)
--- src/test_lark_extractor.py
+++ src/test_lark_extractor.py
@@ -7,31 +7,51 @@ test_cases = [
     ("Akrasan Squire", []),
     ("Abhorrent Overlord", [MagicToken(colors=["B"], types=[
      "Creature"], subtypes=["Harpy"], power="1", toughness="1")]),
-    ("Abstruse Interference",  [MagicToken(colors=[], types=[
-        "Creature"], subtypes=["Eldrazi", "Scion"], power="1", toughness="1", text="Sacrifice this creature: Add {C}.")]),
+    ("Abstruse Interference",  [MagicToken(colors=[], types=["Creature"], subtypes=[
+     "Eldrazi", "Scion"], power="1", toughness="1", text="Sacrifice this creature: Add {C}.")]),
     ("Abzan Ascendancy",  [MagicToken(colors=["W"], types=[
-        "Creature"], subtypes=["Spirit"], power="1", toughness="1")]),
+     "Creature"], subtypes=["Spirit"], power="1", toughness="1")]),
     ("Academy Manufactor", []),
     ("Acorn Catapult", [MagicToken(colors=["G"], types=[
-        "Creature"], subtypes=["Squirrel"], power="1", toughness="1")]),
+     "Creature"], subtypes=["Squirrel"], power="1", toughness="1")]),
     ("Acorn Harvest", [MagicToken(colors=["G"], types=[
-        "Creature"], subtypes=["Squirrel"], power="1", toughness="1")]),
+     "Creature"], subtypes=["Squirrel"], power="1", toughness="1")]),
     ("Advent of the Wurm", [MagicToken(colors=["G"], types=[
-        "Creature"], subtypes=["Wurm"], power="5", toughness="5")]),
+     "Creature"], subtypes=["Wurm"], power="5", toughness="5")]),
     ("Adverse Conditions",  [MagicToken(colors=[], types=[
-        "Creature"], subtypes=["Eldrazi", "Scion"], power="1", toughness="1", text="Sacrifice this creature: Add {C}.")]),
+     "Creature"], subtypes=["Eldrazi", "Scion"], power="1", toughness="1", text="Sacrifice this creature: Add {C}.")]),
     ("Aerie Worshippers",  [MagicToken(colors=["U"], types=[
-        "Enchantment", "Creature"], subtypes=["Bird"], power="2", toughness="2")]),
+     "Enchantment", "Creature"], subtypes=["Bird"], power="2", toughness="2")]),
     ("Aether Chaser", [MagicToken(colors=[], types=[
-        "Artifact", "Creature"], subtypes=["Servo"], power="1", toughness="1")]),
+     "Artifact", "Creature"], subtypes=["Servo"], power="1", toughness="1")]),
     ("Aether Mutation", [MagicToken(colors=["G"], types=[
-        "Creature"], subtypes=["Saproling"], power="1", toughness="1")])
+     "Creature"], subtypes=["Saproling"], power="1", toughness="1")]),
+    ("Ajani, Adversary of Tyrants", [MagicToken(colors=["W"], types=[
+     "Creature"], subtypes=["Cat"], power="1", toughness="1")]),
+    ("Ajani, Caller of the Pride", [MagicToken(colors=["W"], types=[
+     "Creature"], subtypes=["Cat"], power="2", toughness="2")]),
+    ("Ajani Goldmane", [MagicToken(colors=["W"],
+     types=["Creature"], subtypes=["Avatar"], text="This creature's power and toughness are each equal to your life total.")]),
+    ("Stangg", [MagicToken(name="Stangg Twin", colors=["R", "G"], supertypes=["Legendary"],
+     types=["Creature"], subtypes=["Human", "Warrior"], power="3", toughness="4")]),
+    ("Estrid, the Masked", [MagicToken(name="Mask", colors=[
+     "W"], types=["Enchantment"], subtypes=["Aura"])]),
+    ("Valduk, Keeper of the Flame", [
+     MagicToken(colors=["R"], types=["Creature"], subtypes=["Elemental"], power="3", toughness="1")]),
+    ("Kemba, Kha Regent", [
+     MagicToken(colors=["W"], types=["Creature"], subtypes=["Cat"], power="2", toughness="2")]),
+    ("Hazezon Tamar", [MagicToken(colors=["R", "G", "W"],
+     types=["Creature"], subtypes=["Sand", "Warrior"], power="1", toughness="1")]),
+    ("Dark Depths", [MagicToken(name="Marit Lage", colors=["B"], supertypes=["Legendary"], types=[
+     "Creature"], subtypes=["Avatar"], power="20", toughness="20")]),
+    ("Drizzt Do'Urden", [MagicToken(name="Guenhwyvar", colors=["G"], supertypes=["Legendary"], types=[
+     "Creature"], subtypes=["Cat"], power="4", toughness="1")])
 ]
 
 
 @ pytest.mark.parametrize("name,expected", test_cases)
 def test_tokens(get_card, extractor, name, expected):
     card = get_card(name)
-    print(getattr(card, "text", "No text"))
-    tokens = extractor.extract(card)
+    print(getattr(card, "text", "(No card text)"))
+    tokens = extractor.extract_from_card(card)
     assert tokens == expected
