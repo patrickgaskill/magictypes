6604397cf0236cc9205f8b9fad7ae970fac957b3
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-06-16 00:10:13 -0500

    Add progress bars

--- src/updatedata.py
+++ src/updatedata.py
@@ -2,6 +2,7 @@ from io import BytesIO
 from zipfile import ZipFile
 import requests
 from rich.console import Console
+from rich.progress import Progress
 from utils import get_data_path
 
 console = Console()
@@ -26,29 +27,41 @@ def fetch_mtgjson() -> None:
     mtgjson_path = get_data_path("mtgjson")
     mtgjson_path.mkdir(parents=True, exist_ok=True)
 
-    for filename in MTGJSON_FILES:
-        with console.status(f"Downloading {filename} from MTGJSON...", spinner="dots"):
+    with Progress(console=console, transient=True) as progress:
+        task = progress.add_task(
+            "Downloading MTGJSON files...", total=len(MTGJSON_FILES)
+        )
+        for filename in MTGJSON_FILES:
             response = requests.get(f"{MTGJSON_BASE_URL}/{filename}.json.zip")
             zipfile = ZipFile(BytesIO(response.content))
             zipfile.extractall(mtgjson_path)
-            console.log(f"MTGJSON {filename} downloaded")
+            file_path = mtgjson_path / f"{filename}.json"
+            progress.console.log(f"MTGJSON {filename} downloaded to {file_path}")
+            progress.advance(task)
+        progress.console.log("✅ [bold green]MTGJSON data updated!")
 
 
 def fetch_scryfall() -> None:
     scryfall_path = get_data_path("scryfall")
     scryfall_path.mkdir(parents=True, exist_ok=True)
 
-    response = requests.get(f"{SCRYFALL_BASE_URL}/bulk-data")
-    bulk_data_items = response.json()
+    with console.status("Fetching bulk data list from Scryfall...", spinner="dots"):
+        response = requests.get(f"{SCRYFALL_BASE_URL}/bulk-data")
+        bulk_data_items = response.json()["data"]
 
-    for bulk_data_item in bulk_data_items["data"]:
-        with console.status(
-            f"Downloading {bulk_data_item['name']} from Scryfall...", spinner="dots"
-        ):
+    with Progress(console=console, transient=True) as progress:
+        task = progress.add_task(
+            "Downloading Scryfall files...", total=len(bulk_data_items)
+        )
+        for bulk_data_item in bulk_data_items:
             data = requests.get(bulk_data_item["download_uri"])
             file_path = scryfall_path / (bulk_data_item["type"] + ".json")
             file_path.write_bytes(data.content)
-            console.log(f"Scryfall {bulk_data_item['name']} downloaded")
+            progress.console.log(
+                f"Scryfall {bulk_data_item['name']} downloaded to {file_path}"
+            )
+            progress.advance(task)
+        progress.console.log("✅ [bold green]Scryfall data updated!")
 
 
 if __name__ == "__main__":
