74a7a5e72387923f6c4bdeb5b19255c3984aab43
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-06-17 12:52:12 -0500

    Convert mtgjsondata to use orjson

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,16 @@
 import time
 from pathlib import Path
 
+import orjson
+
 
 def get_data_path(filename) -> Path:
     return Path(__file__).parent.joinpath("../data/", filename).resolve()
+
+
+def get_data_json(filename) -> any:
+    with get_data_path(filename).open() as f:
+        return orjson.loads(f.read())
 
 
 def make_output_dir() -> Path:
