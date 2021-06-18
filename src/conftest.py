5431c073dc9fc8aa86b3bc16f4520af71f7710af
Patrick Gaskill <patrick.gaskill@gmail.com>
2021-06-18 14:27:37 -0500

    Add tokenextractor to conftest

--- src/conftest.py
+++ src/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 
 from mtgjsondata import MtgjsonData
+from tokenextractor import TokenExtractor
 
 
 @pytest.fixture(scope="session")
 def mtgjsondata():
     return MtgjsonData()
+
+
+@pytest.fixture(scope="session")
+def extractor():
+    return TokenExtractor()
